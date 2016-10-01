2e74994344918a04fe7fe744d43ed924fa8e264d
timkartar <timkartar7879@gmail.com>
2016-10-01 13:55:24 +0530

    Added signup forms

--- app/src/main/java/com/example/findx/findx/StartActivity.java
+++ app/src/main/java/com/example/findx/findx/StartActivity.java
@@ -2,6 +2,15 @@ package com.example.findx.findx;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +18,30 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login1(view);
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login2(view);
+            }
+        });
     }
-}
+    public void login1(View view){
+        Intent intent = new Intent(this,loginhirer.class) ;
+        startActivity(intent);
+    }
+    public void login2(View view){
+        Intent intent = new Intent(this,loginworker.class) ;
+        startActivity(intent);
+    }
+    }
+
+
